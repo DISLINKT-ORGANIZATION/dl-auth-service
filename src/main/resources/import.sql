1be609649398dde56fdccbd7878be183baf22296
verak13 <verakovacevic98@gmail.com>
2022-08-13 11:23:23 +0200

    fix: birthdate type changed to long

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -4,7 +4,7 @@ INSERT INTO authorities (name) VALUES ('ROLE_USER');
 
 insert into administrators (id, email, username, password) values (nextval('person_seq'), 'admin@gmail.com', 'admin', '$2y$12$NFN7DJUX1lFfaDX1tc9/6uBtgls9SZOU9iwjhrlXJc0xr471vgKAK');
 insert into agents (id, email, username, password) values (nextval('person_seq'), 'agent@gmail.com', 'agent', '$2y$12$NFN7DJUX1lFfaDX1tc9/6uBtgls9SZOU9iwjhrlXJc0xr471vgKAK');
-insert into users (id, email, username, password, first_name, last_name, birth_date, gender) values (nextval('person_seq'), 'user@gmail.com', 'user', '$2y$12$NFN7DJUX1lFfaDX1tc9/6uBtgls9SZOU9iwjhrlXJc0xr471vgKAK', 'first', 'last', 'birth', 0);
+insert into users (id, email, username, password, first_name, last_name, birth_date, gender) values (nextval('person_seq'), 'user@gmail.com', 'user', '$2y$12$NFN7DJUX1lFfaDX1tc9/6uBtgls9SZOU9iwjhrlXJc0xr471vgKAK', 'first', 'last', 913507200000, 0);
 
 insert into user_authority (user_id, authority_id) values (1, 1);
 insert into user_authority (user_id, authority_id) values (2, 2);
