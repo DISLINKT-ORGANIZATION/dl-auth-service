72281df771a8b08f778d9dac09dabe1386eafc9d
Natasa Ivanovic <natasaivanovic98@yahoo.com>
2022-08-31 17:24:57 +0200

    feat: filter users

--- src/main/java/dislinkt/authservice/services/AuthenticationService.java
+++ src/main/java/dislinkt/authservice/services/AuthenticationService.java
@@ -1,6 +1,11 @@
 package dislinkt.authservice.services;
 
 import dislinkt.authservice.dtos.*;
+import org.bouncycastle.LICENSE;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
 
 public interface AuthenticationService {
 
@@ -18,4 +23,5 @@ public interface AuthenticationService {
 	
 	boolean checkIfUsernameExists(String username);
 
+    List<PersonDto> filter(String query);
 }
