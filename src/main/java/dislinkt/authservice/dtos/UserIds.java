455fc4602bba505ef24c33f94a47ee29a5b6010e
Natasa Ivanovic <natasaivanovic98@yahoo.com>
2022-09-06 21:32:52 +0200

    feat: filtering users

---
+++ src/main/java/dislinkt/authservice/dtos/UserIds.java
@@ -0,0 +1,15 @@
+package dislinkt.authservice.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserIds {
+
+    List<Long> ids;
+}
