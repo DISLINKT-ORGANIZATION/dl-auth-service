b8c90ce2e08ceab54a62d40e601f4645ee5d01c9
verak13 <verakovacevic98@gmail.com>
2022-09-10 13:36:35 +0200

    feat: added user registration and company registration for agent app

--- src/main/java/dislinkt/authservice/mappers/AgentDtoMapper.java
+++ src/main/java/dislinkt/authservice/mappers/AgentDtoMapper.java
@@ -3,6 +3,7 @@ package dislinkt.authservice.mappers;
 import org.springframework.stereotype.Service;
 
 import dislinkt.authservice.dtos.AgentCreateRequest;
+import dislinkt.authservice.dtos.AgentDto;
 import dislinkt.authservice.dtos.PersonDto;
 import dislinkt.authservice.entities.Agent;
 
@@ -18,8 +19,13 @@ public class AgentDtoMapper {
 		return new Agent(request.getEmail(), request.getUsername(), request.getCompany());
 	}
 
-    public PersonDto toDtoWithToken(Agent agent, String token) {
+	public PersonDto toDtoWithToken(Agent agent, String token) {
 		return new PersonDto(agent.getId(), agent.getUsername(), agent.getEmail(), agent.getCompany(),
 				agent.getAuthorities().get(0).getAuthority(), token);
-    }
+	}
+
+	public AgentDto toAgentDto(Agent agent) {
+		return new AgentDto(agent.getId(), agent.getUsername(), agent.getEmail(),
+				agent.getAuthorities().get(0).getAuthority());
+	}
 }
